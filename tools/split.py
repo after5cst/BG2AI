7d3b1ada678d3e16f635067ed178011db6290533
Joe Marley <joe.marley@live.com>
2018-08-30 06:41:59 -0600

    Converting to JSON more or less, needs work

--- tools/split.py
+++ tools/split.py
@@ -1,16 +1,33 @@
 #! /usr/bin/env python3
 import argparse
+import json
 import logging
 import os
-from pprint import pprint
+import pprint
 import re
 import shutil
 import sys
+
+***
+*** TODO : CONVERT DOUBLE TO SINGLE QUOTES
+***
 
 _this_dir = os.path.dirname(os.path.realpath(__file__))
 
 # This breaks a statement into an if and a then block
 _if_then_regex = r"(?P<statement>IF(?P<if>(.|\n)*?)^THEN$(?P<then>(.|\n)*?)END)"
+
+
+def promote_trigger(source: list, trigger: str) -> list:
+    """Move items matching the trigger regex to the start of the returned list"""
+    result = []
+    r = re.compile(trigger)
+    for item in source:
+        if isinstance(item, str) and r.match(item):
+            result = [item] + result
+        else:
+            result.append(item)
+    return result
 
 
 def split_file(source_file: str, target_dir: str) -> list:
@@ -28,7 +45,8 @@ def split_file(source_file: str, target_dir: str) -> list:
     outputs = []
     for m in r.finditer(source_text):
         count = count + 1
-        output_name = os.path.join(target_dir, "{:03}.txt".format(count))
+        # It's not json... yet.  But it will be after split_if_then.
+        output_name = os.path.join(target_dir, "{:03}.json".format(count))
         with open(output_name, "w") as f:
             f.write(m.groupdict()["statement"])
         outputs.append(output_name)
@@ -45,10 +63,12 @@ def split_if_then(source_file: str) -> dict:
     logging.debug("Read {} bytes".format(len(source_text)))
 
     r = re.compile(_if_then_regex, flags=re.MULTILINE)
-    r_or = re.compile( "OR\((\d+)\)")
+    r_or = re.compile(r"OR\((\d+)\)")
+    r_resp = re.compile(r"RESPONSE #(\d+)")
 
     count = 0
-    output = {"if": [], "then": [], "ActionListEmpty": False}
+    triggers = []
+    actions = []
     for m in r.finditer(source_text):
         count = count + 1
 
@@ -60,29 +80,42 @@ def split_if_then(source_file: str) -> dict:
 
             if 0 == len(line):
                 pass
-            elif 0 == or_count and "ActionListEmpty()" == line:
-                output["ActionListEmpty"] = True
+            # elif 0 == or_count and "ActionListEmpty()" == line:
+            #     output["ActionListEmpty"] = True
             elif or_check:
                 or_count = int(or_check.group(1))
-                output["if"].append( {"OR": []})
+                triggers.append({"OR": []})
             elif or_count > 0:
-                output["if"][-1]["OR"].append(line)
+                triggers[-1]["OR"].append(line)
                 or_count = or_count - 1
             else:
-                output["if"].append(line)
+                triggers.append(line)
 
         # Break then conditions into separate lines.
+        action_list = []
+        response_value = None
         for line in m.group("then").split('\n'):
-            line = line.rstrip()
+            line = line.strip()
+            response_check = r_resp.match(line)
             if 0 == len(line):
                 pass
+            elif response_check:
+                if response_value:
+                    actions.append({response_value: action_list})
+                response_value = response_check.group(1)
+                action_list = []
             else:
-                output["then"].append(line)
+                action_list.append(line)
+        if response_value:
+            actions.append({response_value: action_list})
 
     if count > 1:
         raise RuntimeError("IF/THEN Parse found multiple matches in '{}'"
                            .format(source_file))
-    return output
+
+    # triggers = promote_trigger(triggers, "^HaveSpell")
+    # triggers = promote_trigger(triggers, "^ActionListEmpty")
+    return {"triggers": triggers, "actions": actions}
 
 
 if __name__ == "__main__":
@@ -112,8 +145,6 @@ if __name__ == "__main__":
     files = split_file(source, target)
 
     for file in files:
-        if_then = split_if_then(file)
-        if "030" not in file:
-            continue
-        pprint(if_then)
-        break
+        data = split_if_then(file)
+        with open(file, "w") as fp:
+            json.dump(data, fp, indent=4)
